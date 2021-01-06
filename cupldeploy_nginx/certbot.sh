38709b4cb2beaa58e0ec99964868307e3e642072
Malcolm Mackay <malcolm.mackay121@gmail.com>
2021-01-06 03:52:18 +0000

    Certbot is not working with nginx. It restarts.

---
+++ cupldeploy_nginx/certbot.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+certbot --nginx --agree-tos --email malcolm.mackay121@gmail.com -n --redirect --expand --keep-until-expiring -d latest.cupl.uk
--- cupldeploy_nginx/docker-entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-tail -f /dev/null # keep alive
